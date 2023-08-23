c40d5ccbbafa84c521a8591d2e6b671b1a95fa16
JIIL EUN <129481038+sarasa0310@users.noreply.github.com>
2023-08-23 18:10:17 +0900

    Time: 1 ms (38.61%), Space: 41.7 MB (12.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // 1. m + n 길이의 배열 선언
+        // 1. nums1과 nums2 요소를 병합할 ArrayList 선언
         ArrayList<Integer> list = new ArrayList<>();
 
-        // 2. nums3에 nums1, nums2 차례대로 병합
+        // 2. list에 nums1, nums2 차례대로 병합
         for (int i = 0; i < m; i++) {
             list.add(nums1[i]);
         }
@@ -14,10 +14,10 @@ class Solution {
             list.add(nums2[i]);
         }
 
-        // 3. nums3 오름차순 정렬
+        // 3. list 오름차순 정렬
         Collections.sort(list);
 
-        // 4. nums1을 nums3로 대치
+        // 4. nums1을 list의 요소로 대치
         for (int i = 0; i < list.size(); i++) {
             nums1[i] = list.get(i);
         }
