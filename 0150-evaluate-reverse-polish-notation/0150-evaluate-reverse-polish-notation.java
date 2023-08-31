22d51af67d368cca822d3a6b7bcdade8120246e9
JIIL EUN <129481038+sarasa0310@users.noreply.github.com>
2023-08-31 10:16:39 +0900

    Time: 6 ms (87.16%), Space: 42.9 MB (88.2%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -3,43 +3,43 @@ class Solution {
         // 1. Stack 자료구조 활용
         Stack<Integer> stack = new Stack<>();
 
-        // 2. tokens 배열 전체 순회하면서 stack 업데이트
+        // 2. tokens 배열 전체 순회하면서 stack에 연산 결과 업데이트
         for (String token : tokens) {
             // 2-1. token이 "+"일 때
             if (token.equals("+")) {
                 stack.push(stack.pop() + stack.pop());
             }
             // 2-2. token이 "-"일 때
-            if (token.equals("-")) {
+            else if (token.equals("-")) {
                 stack.push(-(stack.pop() - stack.pop()));
             }
             // 2-3. token이 "*"일 때
-            if (token.equals("*")) {
+            else if (token.equals("*")) {
                 stack.push(stack.pop() * stack.pop());
             }
             // 2-4. token이 "/"일 때
-            if (token.equals("/")) {
+            else if (token.equals("/")) {
                 int first = stack.pop();
                 int second = stack.pop();
                 stack.push(second / first);
             }
             // 2-5. token이 단순 숫자일 때
-            if (isNumeric(token)) {
+            else {
                 stack.push(Integer.valueOf(token));
             }
         }
 
-        // 3. stack에 저장된 마지막 값 리턴
+        // 3. stack에 저장된 마지막 결과 값 리턴
         return stack.pop();
     }
 
-    public static boolean isNumeric(String token) {
-        try {
-            int d = Integer.parseInt(token);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-    
+    // public static boolean isNumeric(String token) {
+    //     try {
+    //         int d = Integer.parseInt(token);
+    //     } catch (NumberFormatException e) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
 }
