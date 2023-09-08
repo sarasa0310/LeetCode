ab136fc5012b0e9e13334e94e134d360ce61215c
JIIL EUN <129481038+sarasa0310@users.noreply.github.com>
2023-09-08 09:48:15 +0900

    Time: 1 ms (47.21%), Space: 44 MB (58.23%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -26,19 +26,18 @@ class Solution {
         // 1. 모든 노드의 값을 담아줄 values
         List<Integer> values = new ArrayList<>();
 
-        // 2. 중위 순회를 통해 list에 값을 순서대로 채우기
-        inOrder(root, values);
+        // 2. 중위 순회를 통해 values에 값을 정렬하여 채우기
+        inOrderSearch(root, values);
 
-        System.out.println(values);
-
-        // 3. list의 k - 1번째 값 리턴
+        // 3. values의 k - 1번째 인덱스 값 리턴
         return values.get(k - 1);
     }
 
-    private void inOrder(TreeNode node, List<Integer> values) {
-        if (node.left != null) inOrder(node.left, values);
+    // 중위 순회
+    private void inOrderSearch(TreeNode node, List<Integer> values) {
+        if (node.left != null) inOrderSearch(node.left, values);
         values.add(node.val);
-        if (node.right != null) inOrder(node.right, values);
+        if (node.right != null) inOrderSearch(node.right, values);
     }
 
 }
