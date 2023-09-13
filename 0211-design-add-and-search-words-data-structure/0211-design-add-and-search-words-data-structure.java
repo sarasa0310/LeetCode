a823f00b93d337da8fa42e85526fb0ca96193cf6
JIIL EUN <129481038+sarasa0310@users.noreply.github.com>
2023-09-13 11:14:02 +0900

    Time: 242 ms (41.77%), Space: 120 MB (5%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -8,30 +8,40 @@ class WordDictionary {
     
     // word의 첫글자를 하나씩 잘라서 key로 만들어 저장하는 방식
     public void addWord(String word) {
-        addWord(word, root);
-    }
+        // addWord(word, root);
+        Node current = root;
 
-    private void addWord(String word, Node node) {
-        // word의 길이가 0이 된다면
-        if (word.length() == 0) {
-            node.isEnd = true; // 해당 단어가 끝임을 표시하고
-            return; // 재귀 종료
+        for (char c : word.toCharArray()) {
+            if (!current.children.containsKey(c)) {
+                current.children.put(c, new Node());
+            }
+            current = current.children.get(c);
         }
 
-        // 첫글자
-        char c = word.charAt(0);
-
-        // 해당 노드의 children에 단어가 없으면 새로 추가
-        if (!node.children.containsKey(c)) {
-            node.children.put(c, new Node());
-        }
-
-        // 다음 노드
-        Node child = node.children.get(c);
-
-        // 단어 하나 잘라서 재귀 호출
-        addWord(word.substring(1), child);
+        current.isEnd = true;
     }
+
+    // private void addWord(String word, Node node) {
+    //     // word의 길이가 0이 된다면
+    //     if (word.length() == 0) {
+    //         node.isEnd = true; // 해당 단어가 끝임을 표시하고
+    //         return; // 재귀 종료
+    //     }
+
+    //     // 첫글자
+    //     char c = word.charAt(0);
+
+    //     // 해당 노드의 children에 단어가 없으면 새로 추가
+    //     if (!node.children.containsKey(c)) {
+    //         node.children.put(c, new Node());
+    //     }
+
+    //     // 다음 노드
+    //     Node child = node.children.get(c);
+
+    //     // 단어 하나 잘라서 재귀 호출
+    //     addWord(word.substring(1), child);
+    // }
     
     public boolean search(String word) {
         return search(word, root, 0);
